992dad1ae9d95573d665598512897838f9f5ef7c
Bang-ipin <ersajogja@gmail.com>
2022-05-27 16:18:27 +0700

    init ciikoo

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+const App = () => {
+    return (
+        <SafeAreaView>
+            <View>
+                <Text>
+                    Helo World
+                </Text>
+            </View>
+        </SafeAreaView>
+    )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
